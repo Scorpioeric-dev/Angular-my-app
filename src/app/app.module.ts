7c9bcf0a6980896989664dd16be8d0610c72ca87
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-01-06 08:01:55 -0700

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonComponent,
-    ListEmployeesComponent
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
