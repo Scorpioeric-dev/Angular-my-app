0641f1c190d5e8d61f841aa9e63eaa76cf9575c7
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-01-06 08:21:25 -0700

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import {RouterModule,Routes} from '@angular/router'
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
 import { CreateEmployeeComponent } from './employees/create-employee.component';
+
+
+//configuring routes in app
+const appRoutes: Routes = [
+  {  path: 'list', component: ListEmployeesComponent},
+  {  path: 'create',component: CreateEmployeeComponent},
+  {  path: '',redirectTo: '/list', pathMatch: 'full'}
+]
+
 
 @NgModule({
   declarations: [
@@ -17,7 +27,8 @@ import { CreateEmployeeComponent } from './employees/create-employee.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
